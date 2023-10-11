0148a1b64484aa6e75485a850573f459dc913785
socolovmaxim <maxim.socolov@isa.utm.md>
2023-10-11 16:22:57 +0300

    lab2var4 for lab3

--- lab24.py
+++ lab24.py
@@ -1,45 +1,56 @@
 import os
 import requests
-from PIL import Image
-import time
+from bs4 import BeautifulSoup
+import urllib.parse
 
-# Создаем папку "dataset", если она не существует
-if not os.path.exists("dataset"):
-    os.mkdir("dataset")
+# Функция для создания папки, если она не существует
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 # Функция для загрузки изображений
-def download_images(search_query, target_folder, num_images):
-    base_url = "https://yandex.ru/images/search"
-    params = {
-        "text": search_query,
-        "type": "photo",
-    }
 
-    # Создаем папку для класса
-    if not os.path.exists(target_folder):
-        os.mkdir(target_folder)
+def download_images(search_query, output_directory, num_pages):
+    create_directory(output_directory)
+    base_url = "https://www.google.com/search" + '?q=' +search_query + '&tbm=isch'
+    print(base_url)
+ 
+    for i in range(num_pages):
 
-    for i in range(num_images):
-        params["p"] = i + 1
-        response = requests.get(base_url, params=params)
+        img_tags = []
+        response = requests.get(base_url)
+        # print(response.url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        print(soup)
+        img_tags = soup.find_all("img", {"class": "DS1iW"}) 
+        print(img_tags)
+       
+        
 
-        if response.status_code == 200:
-            # Ищем URL изображения в HTML-коде страницы
-            image_url = response.text.split('class="serp-item__link i-bem" href="')[1].split('"')[0]
-            image_response = requests.get(image_url)
+        for img_tag in img_tags:
+          
+            img_url = img_tag["src"]
+            img_url = urllib.parse.urljoin(base_url, img_url)
+            # print(img_url)
+            img_data = requests.get(img_url).content
+        
 
-            if image_response.status_code == 200:
-                with open(f"{target_folder}/{str(i).zfill(4)}.jpg", "wb") as f:
-                    f.write(image_response.content)
+            # Создаем имя файла в формате 0000.jpg, 0001.jpg и т.д.
+            filename = f"{i * len(img_tags) + img_tags.index(img_tag):04d}.jpg"
+            file_path = os.path.join(output_directory, filename)
 
-                # Добавляем задержку, чтобы избежать блокировки
-                time.sleep(1)
-            else:
-                print(f"Ошибка при загрузке изображения {i + 1}")
-        else:
-            print(f"Ошибка при поиске изображения {i + 1}")
+            # Записываем изображение в файл
+            with open(file_path, 'wb') as img_file:
+                img_file.write(img_data)
+
 
 # Загрузка изображений роз
-download_images('rose', 'dataset/rose', 1000)
+download_images('rose', 'dataset/rose', 1)
 # Загрузка изображений тюльпанов
-download_images('tulip', 'dataset/tulip', 1000)
+download_images('tulip', 'dataset/tulip', 1)
+
+
+
+#
+
+
