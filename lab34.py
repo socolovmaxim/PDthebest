83f44108622a869264a3b7513a3218ec3049f84a
socolovmaxim <maxim.socolov@isa.utm.md>
2023-10-17 15:32:30 +0300

    lab3.4 ready

--- lab34.py
+++ lab34.py
@@ -2,3 +2,34 @@
 #возвращающую следующий экземпляр (путь к нему) этого класса. Экземпляры идут в 
 #любом порядке, но не повторяются. Когда экземпляры заканчиваются, функция возвращает None.
 
+import os
+import random
+
+# Функция для получения следующего экземпляра класса
+def get_next_instance(class_label, dataset_directory):
+    class_directory = os.path.join(dataset_directory, class_label)
+
+    if not os.path.exists(class_directory):
+        return None
+
+    files = [file for file in os.listdir(class_directory) if file.endswith('.jpg')]
+    if not files:
+        return None
+
+    random.shuffle(files)
+    next_instance = files.pop()
+    file_path = os.path.join(class_directory, next_instance)
+
+    return file_path
+
+# Укажите путь к вашему датасету
+dataset_directory = 'dataset'
+
+# Пример использования функции
+class_label = 'rose'
+next_instance = get_next_instance(class_label, dataset_directory)
+
+if next_instance is not None:
+    print(f"Следующий экземпляр класса '{class_label}': {next_instance}")
+else:
+    print(f"Класс '{class_label}' закончился.")
