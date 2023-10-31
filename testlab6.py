76ffaa7b460475e1171c8f751d277416a9d235f6
socolovmaxim <maxim.socolov@isa.utm.md>
2023-10-31 16:14:51 +0200

    lab5-6

---
+++ testlab6.py
@@ -0,0 +1,24 @@
+import unittest
+import telebot
+from tgbot import bot 
+from tgbot import send_silver_sword_info  
+
+class TestBotFunctionality(unittest.TestCase):
+
+    # Тест для функции send_silver_sword_info
+    def test_send_silver_sword_info(self):
+        # Создаем объект сообщения для имитации сообщения от пользователя
+        class Message:
+            def __init__(self, chat_id):
+                self.chat_id = chat_id
+                self.text = "Серебреные Мечи"
+
+        # Имитируем сообщение от пользователя
+        message = Message(713901266)  
+        result = send_silver_sword_info(message.chat_id, 0, bot)
+
+        # Проверяем, что результатом является строка (не None)
+        self.assertIsInstance(result, str)
+
+if __name__ == '__main__':
+    unittest.main()
